bee7597763504a881d59c862c81ee0a54b552c46
zdr <2743813026@qq.com>
2026-03-17 20:12:09 +0800

    完成新增员工功能

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -22,6 +25,27 @@ public class GlobalExceptionHandler {
     public Result exceptionHandler(BaseException ex){
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
+    }
+    /**
+     * 捕获数据库异常
+     * 数据库已存在的用户名
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        String message = ex.getMessage();
+        //Cause: java.sql.SQLIntegrityConstraintViolationException:
+        // Duplicate entry '广依诺' for key 'employee.idx_username'
+        //捕获的异常
+        if (message.contains("Duplicate entry")) {
+            String[] split = message.split(" ");//信息用空格隔开分成字符串数组
+            String username = split[2];//获取用户名，索引为2
+            String msg = username + MessageConstant.ALREADY_EXISTS;//拼接错误信息
+            return Result.error(msg);//返回结果
+        }else{
+            return Result.error(MessageConstant.UNKNOWN_ERROR);
+        }
     }
 
 }
